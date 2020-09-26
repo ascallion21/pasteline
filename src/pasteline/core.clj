f8e3b9ba0083c3174eccdcc5b23f1ef964f0cf4a
Graham Scanlon <gscanlon21@gmail.com>
2020-09-25 20:54:25 -0500

    fix: rate limit pasting to once every second

--- src/pasteline/core.clj
+++ src/pasteline/core.clj
@@ -4,6 +4,7 @@
 (require 'pasteline.clipboard)
 (require 'pasteline.robot)
 (require 'keymaster.core)
+(import java.util.concurrent.locks.ReentrantLock)
 
 (defn paste []
   ((let [text (clojure.string/replace (pasteline.clipboard/clipget) #"[\r\n]+", "")]
@@ -13,5 +14,13 @@
 (defn -main
   "Strips newlines from a text block when pasting via ctrl-alt-v"
   [& args]
-  (let [provider (keymaster.core/make-provider)]
-    (keymaster.core/register provider "control alt V" #((pasteline.core/paste)))))
+  (let [provider (keymaster.core/make-provider)
+        lock (ReentrantLock.)]
+    (keymaster.core/register provider "control alt V"
+                             #((if (.tryLock lock)
+                                 (try
+                                   ((pasteline.core/paste)
+                                    (Thread/sleep 1000))
+                                   (finally (.unlock lock)))
+                                 ((println "Keybinding recently invoked, ignoring")))))))
+
