4624123891b7def473753ac2ceed030c3a4ca9a5
Graham Scanlon <gscanlon21@gmail.com>
2021-03-27 21:40:34 -0500

    feat: allow passing the keybinding, regex and replacement through the cmd line

--- src/pasteline/core.clj
+++ src/pasteline/core.clj
@@ -1,5 +1,4 @@
 (ns pasteline.core
-  (:gen-class)
   (:require [clojure.string :as s]
             [pasteline.clipboard :as clipboard]
             [pasteline.robot :as robot]
@@ -10,16 +9,18 @@
    2. Replaces all occurences of newlines with {replacementText}
    3. Sets the modified text onto the clipboard
    4. Invokes the ctrl-V paste command"
-  [& {:keys [replacementText] :or {replacementText ""}}]
+  [replacementRegex, replacementText]
   (if-let [clipboardText (clipboard/clipget)]
-    (let [formattedText (s/replace clipboardText #"[\r\n]+", replacementText)]
+    (let [formattedText (s/replace clipboardText replacementRegex, replacementText)]
       (clipboard/clipset formattedText)
       (robot/pastetext))
     (println "There is nothing to paste")))
 
 (defn -main
-  "Strips newlines from a text block when pasting via ctrl-alt-v
-  TODO - alter this to accept a custom value for replacementText or the keybinding"
+  "Strips newlines from a text block when pasting via ctrl-alt-v"
   [& args]
-  (let [provider (keymaster.core/make-provider)]
-    (keymaster.core/register provider "control alt V" #(onPaste))))
+  (let [provider (keymaster.core/make-provider)
+        keybinding (nth args 0 "control alt V")
+        replacementRegex (re-pattern (nth args 1 "[\r\n]+"))
+        replacementText (nth args 2 "")]
+    (keymaster.core/register provider keybinding #(onPaste replacementRegex replacementText))))
